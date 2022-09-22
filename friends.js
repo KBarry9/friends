dff28e4ab2645ae7639969f478b22806d715247a
Kelly Barry <kleemuir@gmail.com>
2022-09-22 14:44:06 -0600

    My Friends Commit

--- friends.js
+++ friends.js
@@ -1,4 +1,8 @@
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-for (var i = 0; i > 30; i++) {
-  console.log(i);
-}
+  for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] < 60) {
+      console.log(numbers[i]);
+    } else if (numbers[i] > 30) {
+      console.log(numbers[i]);
+    }
+  }
